2d32d2467de3b387b44ca0411836f2dee68c3e9a
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2023-05-17 13:20:19 -0300

    test: simple Michelson test implemented

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using Interferometers
 using Test
 
 @testset "Interferometers.jl" begin
-    # Write your tests here.
+
+    @test michelson(0, 1, 1) == 2
+    @test michelson(0.0, 1.0, 1.0) == 2
+    @test michelson(deg2rad(90), 1, 1) == 1
+    @test michelson(deg2rad(90), 0, 0) == 0
+
 end
